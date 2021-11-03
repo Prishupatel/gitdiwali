6b9d133d72e3b7ae7abcc1f7279bc0ea67a983f3
prishupatel <prishupatel218@gmail.com>
2021-11-03 12:00:22 -0400

    bug fixed in testing branch

---
+++ src/Project1/DiwaliGift.java
@@ -0,0 +1,9 @@
+package Project1;
+
+public class DiwaliGift {
+    public static void main(String[] args) {
+        System.out.println("Happy Diwali");
+
+        System.out.println("Happy new year" + "Happy Diwali");
+    }
+}
